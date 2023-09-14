c90a70f8e1844f9c48438ccb819ce20f8b426503
sunny90100 <sunithanamala1503@gmail.com>
2023-09-14 10:34:05 -0700

    added new file MultiLevelInheritance

---
+++ MultiLevelInheritance.java
@@ -0,0 +1,89 @@
+C:\Users\ad\Desktop\javawork\Demo1\src\objectoriented
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package objectoriented;
+class Vehicle{
+	public void CarWashGuideLines() {
+		System.out.println("use water to wash");
+	}
+}
+class  TwoWheeler extends Vehicle{
+	public void Services() {
+		System.out.println("service the two wheelers once for 2000km of ride");
+	}
+}
+class KTM extends TwoWheeler{
+	public void RidingSafety() {
+		System.out.println("wear helmet");
+		
+	}
+}
+
+public class MultiLevelInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		KTM k=new KTM();
+		k.CarWashGuideLines();
+		k.Services();
+		k.RidingSafety();
+		
+
+	}
+
+}
+
